cefa586af338d052239948981eadb39a77a9c8a2
andrei alekseev <aimdragman2@yandex.ru>
2023-05-02 18:58:46 +0300

    feat: udpate directory page with directory component and category-item component

---
+++ src/components/category-item/category-item.component.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react";
+
+import type { Category } from "../../types/category";
+
+import './category-item.styles.scss';
+
+type Props = {
+  category: Category
+}
+
+const CategoryItem: FC<Props> = ({ category }): JSX.Element => {
+  const { imageUrl, title } = category;
+
+  return (
+    <div className="category-container">
+      <div className="background-image" style={{
+        backgroundImage: `url(${imageUrl})`
+      }}></div>
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  )
+}
+
+export default CategoryItem;
---
+++ src/components/directory/directory.component.tsx
@@ -0,0 +1,23 @@
+import { FC } from "react";
+
+import CategoryItem from "../category-item/category-item.component";
+
+import type { Category } from "../../types/category";
+
+import './directory.styles.scss';
+
+type Props = {
+  categories: Readonly<Category[]>
+}
+
+const Directory: FC<Props> = ({categories}) => {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  )
+}
+
+export default Directory;
